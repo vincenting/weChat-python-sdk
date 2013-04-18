124e65b4d06e12b987d331783f176e8797e85b38
Vincent <homerdd@gmail.com>
2013-04-18 09:41:13 +0800

    更新简单使用事例

--- demo.py
+++ demo.py
@@ -1,3 +1,18 @@
+# coding=utf-8
+"""
+首先需要在微信公共平台申请帐号 mp.weixin.qq.com
+之后才可以使用下事例
+"""
 __author__ = 'Vincent Ting'
 
 from weChat.client import Client
+
+client = Client(email='xx@gmail.com', password='yourPsw')
+
+#demo  推送文字信息,其中sendto为关注该帐号的某用户的fakeId
+
+client.sendTextMsg('xxxxxxx', 'Hello world')
+
+#demo  推送图片信息,其中sendto为关注该帐号的某用户的fakeId，img为图片的文件路径
+
+client.sendImgMsg('xxxxxxx', 'E:/demo.png')
